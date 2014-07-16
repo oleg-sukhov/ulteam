0d14d334048bc76eeb3df111b31b325d89e561e0
oleg_sukhov <oleg.sukhov@email.ua>
2014-07-16 22:42:37 +0300

    Change build script

---
+++ ulteam-model/src/main/resources/ua/vn/os/ulteam/model/sql/dbScript.sql
@@ -0,0 +1,23 @@
+﻿/*check if database exist*/
+DO
+$do$
+BEGIN
+IF EXISTS (SELECT 1 FROM pg_database WHERE datname = 'ulteam') THEN
+   RAISE NOTICE 'Database <ulteam> exist';
+ELSE 
+   CREATE DATABASE ulteam ENCODING 'UTF-8';
+   RAISE NOTICE 'Database was created';
+END IF;
+
+/*create news table*/
+CREATE TABLE IF NOT EXISTS news (
+    id                   INTEGER NOT NULL,
+    title                TEXT NOT NULL,
+    modification_time    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    views                INTEGER,
+    new_content          TEXT,
+    picture              BYTEA,
+    PRIMARY KEY (id)           
+);
+END
+$do$
