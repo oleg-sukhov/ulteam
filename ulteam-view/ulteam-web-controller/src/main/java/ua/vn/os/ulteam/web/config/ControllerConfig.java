91d03e1004bc853484885c4c331910bbc665f4bb
oleg_sukhov <oleg.sukhov@email.ua>
2014-08-07 22:17:44 +0300

    Add Bootstrap library

--- ulteam-view/ulteam-web-controller/src/main/java/ua/vn/os/ulteam/web/config/ControllerConfig.java
+++ ulteam-view/ulteam-web-controller/src/main/java/ua/vn/os/ulteam/web/config/ControllerConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
@@ -17,12 +20,12 @@ import ua.vn.os.ulteam.service.config.ServiceConfig;
 @EnableWebMvc
 @ComponentScan(basePackages = "ua.vn.os.ulteam.web.controller")
 @Import({ServiceConfig.class})
-public class ControllerConfig {
+public class ControllerConfig extends WebMvcConfigurerAdapter {
 
     @Bean
     public ServletContextTemplateResolver templateResolver() {
         ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
-        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setPrefix("/WEB-INF/templates/");
         resolver.setSuffix(".html");
         resolver.setTemplateMode("HTML5");
         resolver.setCacheable(false);
@@ -42,5 +45,10 @@ public class ControllerConfig {
         viewResolver.setTemplateEngine(templateEngine());
         viewResolver.setCharacterEncoding("UTF-8");
         return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
 }
