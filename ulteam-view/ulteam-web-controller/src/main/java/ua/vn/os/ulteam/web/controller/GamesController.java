72d927050bf4b80477d3dd0c8f298197b707cdb5
oleg_sukhov <oleg.sukhov@email.ua>
2014-10-06 23:35:56 +0300

    Implement games control bar

--- ulteam-view/ulteam-web-controller/src/main/java/ua/vn/os/ulteam/web/controller/GamesController.java
+++ ulteam-view/ulteam-web-controller/src/main/java/ua/vn/os/ulteam/web/controller/GamesController.java
@@ -1,10 +1,20 @@
 package ua.vn.os.ulteam.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author os
  */
 @Controller
 public class GamesController {
+
+    @RequestMapping(value = "/games", method = RequestMethod.GET)
+    public ModelAndView getGamesInCurrentSeason() {
+        ModelAndView modelAndView = new ModelAndView("games");
+        return modelAndView;
+    }
 }
