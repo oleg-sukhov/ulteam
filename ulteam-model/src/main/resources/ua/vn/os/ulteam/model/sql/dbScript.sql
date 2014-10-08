6ee5a8879070036f6c78ce55f0d46f35fa2466d1
oleg_sukhov <oleg.sukhov@email.ua>
2014-10-08 23:29:16 +0300

    Change db script to add tables for game, season, tournament,team

--- ulteam-model/src/main/resources/ua/vn/os/ulteam/model/sql/dbScript.sql
+++ ulteam-model/src/main/resources/ua/vn/os/ulteam/model/sql/dbScript.sql
@@ -4,20 +4,20 @@ $do$
 BEGIN
 IF EXISTS (SELECT 1 FROM pg_database WHERE datname = 'ulteam') THEN
    RAISE NOTICE 'Database <ulteam> exist';
-ELSE 
+ELSE
    CREATE DATABASE ulteam ENCODING 'UTF-8';
    RAISE NOTICE 'Database was created';
 END IF;
 
 --/*create news table*/
 CREATE TABLE IF NOT EXISTS news (
-    id                   BIGSERIAL NOT NULL,
-    title                VARCHAR(255) NOT NULL,
-    short_description    TEXT NOT NULL,
-    modification_date    BYTEA NOT NULL,
-    views                BIGINT NOT NULL,
-    news_content         TEXT NOT NULL,
-    picture              BYTEA NOT NULL,
+    id                  BIGSERIAL NOT NULL,
+    title               VARCHAR(255) NOT NULL,
+    short_description   TEXT NOT NULL,
+    modification_date   BYTEA NOT NULL,
+    views               BIGINT NOT NULL,
+    news_content        TEXT NOT NULL,
+    picture             BYTEA NOT NULL,
     PRIMARY KEY(id)
 );
 
@@ -41,6 +41,40 @@ CREATE TABLE IF NOT EXISTS Photo (
     album_id             BIGINT NOT NULL,
     PRIMARY KEY(id),
     FOREIGN KEY(album_id) REFERENCES PhotoAlbum(id)
+);
+
+/*create table for storing information about games*/
+CREATE TABLE IF NOT EXISTS Game(
+    id BIGSERIAL      NOT NULL,
+    owner_team        BIGINT NOT NULL,
+    guest_team        BIGINT NOT NULL,
+    onwner_team_goals SMALLINT,
+    guest_team_goals  SMALLINT,
+    game_date         DATE,
+    season            BIGINT NOT NULL,
+    tournament        BIGINT NOT NULL,
+		tour              VARCHAR(50),
+    game_details      BIGINT
+    PRIMARY KEY(id),
+    FOREIGN KEY(owner_team) REFERENCES Team(id),
+    FOREIGN KEY(guest_team) REFERENCES Team(id)
+    FOREIGN KEY(season)
+);
+
+CREATE TABLE IF NOT EXISTS Team(
+    id BIGSERIAL NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Season(
+    id BIGSERIAL NOT NULL,
+    name VARCHAR(10) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS Tournament(
+    id BIGSERIAL NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    season BIGINT NOT NULL,
 );
 END
 $do$
